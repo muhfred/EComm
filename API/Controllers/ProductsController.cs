c0f75ead87bd9e10b893e42f674e57a39cfb1bee
muhfred <muhfred@outlook.com>
2020-07-28 18:32:13 +0200

    initial structure

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -11,6 +14,24 @@ namespace API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private readonly EcommContext _context;
 
+        public ProductsController(EcommContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> Get(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
     }
 }
---
+++ Infrastructure/Data/EcommContext.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class EcommContext : DbContext
+    {
+        public EcommContext(DbContextOptions<EcommContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
